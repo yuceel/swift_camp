7159c5c31f9b216804999ee5c2d6bcdc06750a49
yuceel <muhammetyucel.diler@gmail.com>
2024-12-26 23:57:07 +0300

    CL - Protocol

--- SwiftBasics/Protocols.swift
+++ SwiftBasics/Protocols.swift
@@ -9,7 +9,7 @@ protocol Animal {
 // struct and classes can get inheritance from protocols. 
 
 struct Bird: Animal{
- var legCount: Int = 4    
+ var legCount: Int = 2   
  var diet: String = "omnivore"
  var wingCount: Int = 2
  var canFly: Bool = true
@@ -33,20 +33,76 @@ var haydut: CannotFlyableAnimals = CannotFlyableAnimals(legCount: 4, diet: "herb
 print(haydut.canFly)
 var haydutsWife: CannotFlyableAnimals = CannotFlyableAnimals(legCount: 4, diet: "herbivor")
 
-protocol CountOfBreeding{
-    func NumberOfBabies(dog1: Animal, dog2: Animal) -> Int 
+//BreedingManager class can be used encapsulate to breed logics. This allows to centralize the management plus modificate to easily if needed.
 
-}
-
-class BabiesCount:CountOfBreeding {
-    func NumberOfBabies(dog1: any Animal, dog2: any Animal) -> Int {
-     
-         let babyNumber:Int = 4
-         return babyNumber
-        
+class BreedingManager{
+    func numberOfBabies(animal1:Animal, animal2: Animal) -> Int{ 
+        let babies: Int
+        if(animal1.legCount != animal2.legCount) {
+            print("these animals cannot be mated")
+            return -1
+        }
+        else if(animal1.legCount == 2) {
+            babies = 2
+            return babies
+        }
+        babies = 4
+        return  babies
     }
 }
 
-let babies = BabiesCount() 
+class Animals : Animal {
+     
+    var legCount: Int 
+    var diet: String 
+    var breedist: BreedingManager
 
+    init (legCount:Int, diet: String, breedist: BreedingManager) {
+        self.legCount = legCount
+        self.diet = diet
+        self.breedist = breedist
+    }   
 
+    func numberOfBabies(animal1: Animal, animal2: Animal) -> Int {
+        return breedist.numberOfBabies(animal1: self.self, animal2: animal2)
+    }
+}
+
+let breedingManager = BreedingManager()
+
+let cimri = Animals(legCount: 2, diet: "herbivore", breedist: breedingManager)
+
+let babiesOfOsmanAndCimri = cimri.numberOfBabies(animal1: cimri, animal2: osman)
+
+print(babiesOfOsmanAndCimri)
+// Adding new proporties with existing class using extensions. 
+protocol BreedingMechanism {
+    var explainBreedingMechanism: String {get}
+
+}
+extension BreedingManager: BreedingMechanism {
+    var explainBreedingMechanism: String {
+    """
+    To mate two animals, they both must have same number of legs. 
+    For example, you can mate a dinosour and a bird because their leg numbers are 2. 
+    But you can't mate an ant and a crocodile. Ants have 6, crocodiles have 4 legs.
+    """
+    }
+}
+
+let mateCimri: String = cimri.breedist.explainBreedingMechanism
+
+print(mateCimri)
+
+protocol babyDog: BreedingMechanism {
+    var babiesOfDogs :String {get}
+}
+
+extension BreedingManager : babyDog{
+    var babiesOfDogs: String {
+        "🐶🐶🐶🐶"
+    }
+}
+
+let puppysOfCimri: String = cimri.breedist.babiesOfDogs
+print(puppysOfCimri)
