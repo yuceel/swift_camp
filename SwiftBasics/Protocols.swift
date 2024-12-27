f5391994412534a9efab02bb1bd7458c0a1b307a
yuceel <muhammetyucel.diler@gmail.com>
2024-12-27 04:08:29 +0300

    fix: refactored structure properly

--- SwiftBasics/Protocols.swift
+++ SwiftBasics/Protocols.swift
@@ -1,108 +1,143 @@
-
 // Protocol can be defined a following way.
 
 protocol Animal {
-    var legCount:Int {get set}
-    var diet :String {get set}   
+  
+    var age: Int {get}
+    var kind: String {get}
+    var gender: String {get}
+    func makeNoise() -> String
+    var isHungry: Bool {get} 
+    var isInfertile: Bool {get}
+
 }
 
-// struct and classes can get inheritance from protocols. 
-
-struct Bird: Animal{
- var legCount: Int = 2   
- var diet: String = "omnivore"
- var wingCount: Int = 2
- var canFly: Bool = true
+protocol DomesticAnimal: Animal {
+    
+    var name: String {get}
+    var vaccineSchedule: String {get}
+    var chipNumber: Int {get}
+    var isWalkPublic: Bool {get}
+    func feed() -> String
+    
 }
-
-let osman:Bird = Bird()
-
-print(osman.canFly)
-
-class CannotFlyableAnimals: Animal {
-    var legCount: Int
-    var diet: String
-    var canFly: Bool = false
-
-    init(legCount: Int, diet: String ) {
-        self.legCount = legCount
-        self.diet = diet
-    }
-}
-var haydut: CannotFlyableAnimals = CannotFlyableAnimals(legCount: 4, diet: "herbivor")
-print(haydut.canFly)
-var haydutsWife: CannotFlyableAnimals = CannotFlyableAnimals(legCount: 4, diet: "herbivor")
-
 //BreedingManager class can be used encapsulate to breed logics. This allows to centralize the management plus modificate to easily if needed.
 
 class BreedingManager{
     func numberOfBabies(animal1:Animal, animal2: Animal) -> Int{ 
-        let babies: Int
-        if(animal1.legCount != animal2.legCount) {
-            print("these animals cannot be mated")
-            return -1
-        }
-        else if(animal1.legCount == 2) {
-            babies = 2
+        
+        var babies: Int = Int.random(in: 1..<5)
+
+        if(animal1.kind == animal2.kind && !(animal1.isInfertile && animal2.isInfertile)) {
+            print("these animals can be mated and they can be has babies")
             return babies
         }
-        babies = 4
+
+        babies = 0
+
         return  babies
     }
 }
+// Class can get inheritance from protocol
+class Cat: DomesticAnimal {
+    var isWalkPublic: Bool
+    var breeding: BreedingManager
+    var isInfertile: Bool
+    var isHungry: Bool
+    var name: String
+    var vaccineSchedule: String
+    var chipNumber: Int
 
-class Animals : Animal {
-     
-    var legCount: Int 
-    var diet: String 
-    var breedist: BreedingManager
+    func feed() -> String {
+        if(isHungry) {
+            let hungryCat: String = "The food bowl fulled with Whiskas"
+            return hungryCat
+        }
+        let fullCat: String = "There is still some food in bowl."
+        return fullCat
+    }
 
-    init (legCount:Int, diet: String, breedist: BreedingManager) {
-        self.legCount = legCount
-        self.diet = diet
-        self.breedist = breedist
-    }   
+    var age: Int
+    var kind: String
+    var gender: String
 
-    func numberOfBabies(animal1: Animal, animal2: Animal) -> Int {
-        return breedist.numberOfBabies(animal1: self.self, animal2: animal2)
+    func makeNoise() -> String {
+        if(isHungry) {
+            return "miiyav miiiyav"
+        }
+        return "mirmirrr"
+    }
+
+    init(name: String, vaccineSchedule: String, chipNumber: Int, age: Int, kind: String, gender: String, isHungry: Bool, isInfertile: Bool, breeding: BreedingManager, isWalkPublic: Bool) {
+   
+    self.name = name
+    self.vaccineSchedule = vaccineSchedule
+    self.chipNumber = chipNumber
+    self.age = age
+    self.kind = kind
+    self.gender = gender
+    self.isHungry = isHungry
+    self.isInfertile = isInfertile
+    self.breeding = breeding
+    self.isWalkPublic = isWalkPublic
     }
 }
 
-let breedingManager = BreedingManager()
+//extensions can be written on protocol.
 
-let cimri = Animals(legCount: 2, diet: "herbivore", breedist: breedingManager)
+extension DomesticAnimal {
+    
+    func wearingMuzzle() -> String{
+        return isWalkPublic ? "While taking a ride in public must wear muzzle" : "No need to wear muzzle at home"
 
-let babiesOfOsmanAndCimri = cimri.numberOfBabies(animal1: cimri, animal2: osman)
-
-print(babiesOfOsmanAndCimri)
-// Adding new proporties with existing class using extensions. 
-protocol BreedingMechanism {
-    var explainBreedingMechanism: String {get}
-
-}
-extension BreedingManager: BreedingMechanism {
-    var explainBreedingMechanism: String {
-    """
-    To mate two animals, they both must have same number of legs. 
-    For example, you can mate a dinosour and a bird because their leg numbers are 2. 
-    But you can't mate an ant and a crocodile. Ants have 6, crocodiles have 4 legs.
-    """
     }
+
 }
 
-let mateCimri: String = cimri.breedist.explainBreedingMechanism
+class Lion: Animal {
 
-print(mateCimri)
-
-protocol babyDog: BreedingMechanism {
-    var babiesOfDogs :String {get}
-}
-
-extension BreedingManager : babyDog{
-    var babiesOfDogs: String {
-        "🐶🐶🐶🐶"
+    var age: Int
+    var kind: String
+    var gender: String
+    func makeNoise() -> String {
+     if(isHungry) {
+        return "RRrrrRrRrrRRRR"
+     } 
+        return ""  
     }
+
+    var isHungry: Bool
+    var isInfertile: Bool
+
+internal init(age: Int, kind: String, gender: String, isHungry: Bool, isInfertile: Bool) {
+   
+    self.age = age
+    self.kind = kind
+    self.gender = gender
+    self.isHungry = isHungry
+    self.isInfertile = isInfertile
+    
+    }
+
 }
 
-let puppysOfCimri: String = cimri.breedist.babiesOfDogs
-print(puppysOfCimri)
+let breedingManager: BreedingManager = BreedingManager()
+
+
+let catOne : Cat = Cat(name: "osman", vaccineSchedule: "Completed.", chipNumber: 1111, age: 3, kind: "british", gender: "male", isHungry: true, isInfertile: false, breeding: breedingManager, isWalkPublic: true)
+let catTwo : Cat = Cat(name: "keltos", vaccineSchedule: "Completed", chipNumber: 1112, age: 2, kind: "british", gender: "female", isHungry: false, isInfertile: false, breeding: breedingManager, isWalkPublic: false)
+
+let lionOne: Lion = Lion(age: 3, kind: "lion", gender: "male", isHungry: true, isInfertile: true)
+let lionTwo: Lion = Lion(age: 4, kind: "lion", gender: "male", isHungry: false, isInfertile: true)
+
+let catBabies: Int = breedingManager.numberOfBabies(animal1: catOne, animal2: catTwo)
+
+let lionBabies: Int = breedingManager.numberOfBabies(animal1: lionOne, animal2: lionTwo)
+
+let forbiddenBabies: Int = breedingManager.numberOfBabies(animal1: catOne, animal2: lionTwo)
+
+print("First cat is \(catOne.name), second cat is \(catTwo.name), first lion is \(lionOne), second lion is \(lionTwo), the number of babies that cats have \(catBabies), the number of babies that lions have \(lionBabies), the unexpected babies that cat and lion have \(forbiddenBabies)")
+
+let walkingCat: String = catOne.wearingMuzzle()
+let homeCat: String = catTwo.wearingMuzzle()
+
+print("\(walkingCat) \n \(homeCat)")
