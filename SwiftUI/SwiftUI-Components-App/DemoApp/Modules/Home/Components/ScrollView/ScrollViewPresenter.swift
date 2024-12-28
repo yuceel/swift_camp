ddc6952838f8673e569f185919cae4417e81958e
Büşra Gedikoğlu <84907307+busragedikoglu@users.noreply.github.com>
2024-12-28 22:18:41 +0300

    fix : ScrollViewPresenter issue

--- SwiftUI/SwiftUI-Components-App/DemoApp/Modules/Home/Components/ScrollView/ScrollViewPresenter.swift
+++ SwiftUI/SwiftUI-Components-App/DemoApp/Modules/Home/Components/ScrollView/ScrollViewPresenter.swift
@@ -1,7 +1,26 @@
-//
-//  ScrollViewPresenter.swift
-//  DemoApp
-//
-//  Created by Büşra Gedikoğlu on 28.12.2024.
-//
+import Foundation
+import SwiftUI
 
+final class ScrollViewPresenter: ObservableObject {
+
+    // MARK: - Properties
+
+    private let wireframe: ScrollViewWireframeInterface
+    
+    //ScrollView properties
+    @Published var scrollAxisHorizontal: Axis.Set = .horizontal
+    @Published var scrollAxisVertical: Axis.Set = .vertical
+    @Published var showsIndicator: Bool = true
+
+    // MARK: - Lifecycle
+
+    init(wireframe: ScrollViewWireframeInterface) {
+        self.wireframe = wireframe
+    }
+
+    // MARK: - Navigation
+
+    func goBack() {
+        wireframe.goBack()
+    }
+}
