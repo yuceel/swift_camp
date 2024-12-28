b7368afb52c376a1260a73cb2d22243befc44eab
Ali Akkaya <117384310+alakkaya@users.noreply.github.com>
2024-12-28 21:14:14 +0300

    add: navigation stack view

---
+++ SwiftUI/SwiftUI-Components-App/DemoApp/Modules/Home/Components/HStack/NavigationStack 2/NavigationStackWireframe.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+final class NavigationStackWireframe:BaseWireframe<LazyHostingViewController<NavigationStackView>> {
+    
+    // MARK: - Module setup -
+    
+    init() {
+        let moduleViewController = LazyHostingViewController<NavigationStackView>(isNavigationBarHidden: true)
+        super.init(viewController: moduleViewController)
+
+        let presenter = Naviga tionStackPresenter(wireframe: self)
+        
+        moduleViewController.rootView =  NavigationStackView(presenter: presenter)
+    }
+}
+
+// MARK: - Extensions -
+
+extension NavigationStackWireframe:NavigationStackWireframeInterface{
+    func fetchItems() {
+        //code
+    }
+    
+    func goBack() {
+        navigationController?.popViewController(animated: true)
+    }
+}
