364df16afa2f6a7daa9b82116abe70771d4ae4da
Gürkan Fikret Günak <52853374+gurkanfikretgunak@users.noreply.github.com>
2024-12-30 19:43:18 +0300

    add: integrate OneSignal SDK with entitlements and background modes for remote notifications

--- SwiftUI/SwiftUI-Components-App/DemoApp/AppDelegate.swift
+++ SwiftUI/SwiftUI-Components-App/DemoApp/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
+import OneSignalFramework
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, AppWindowHandler {
 
     // MARK: - Public properties -
@@ -11,18 +12,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AppWindowHandler {
         didSet { initializers.forEach { $0.initialize() } }
     }
 
-    // MARK: Lifecycle
+    // MARK: - Lifecycle -
 
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        
+        // OneSignal initialization
+        OneSignal.initialize("9663e4ef-0e81-4ff2-8ae0-d37dc5225b9a", withLaunchOptions: launchOptions)
+        
+        OneSignal.Notifications.requestPermission({ accepted in
+            print("User accepted notifications: \(accepted)")
+        }, fallbackToSettings: true)
+        
+        // Configure initializers
         initializers = StartupInitializationBuilder()
             .setAppDelegate(self)
             .build(with: launchOptions)
-
+        
         return true
     }
-
 }
-
