43e2c34bc5cb6c4a28d3c2e850b609c111e45ed1
melihalkbk <melihalkbk@gmail.com>
2024-12-30 22:36:45 +0300

    fix:assets bug and add some images

--- SwiftUI/SwiftUI-Components-App/DemoApp/Modules/Home/Components/Image/ImageView.swift
+++ SwiftUI/SwiftUI-Components-App/DemoApp/Modules/Home/Components/Image/ImageView.swift
@@ -1,23 +1,34 @@
-
-
 import SwiftUI
 
 struct ImageView: View {
     @ObservedObject var presenter: ImagePresenter
+    let imageNames = ["atatürk", "ata2", "ata3", "ata4", "ata5", "ata6"]
+    
     var body: some View {
-        Image("atatürk")
-            .resizable()
-            .scaledToFit()
-            .frame(width: 200, height: 200)
-        
-        Button("Go Back") {
+        VStack {
+            // Başlık
+            Text("A Small Atatürk Exhibition")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.bottom, 10)
+            
+            // Görselleri bir Grid ile düzenlemek için LazyVGrid
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 20), count: 2), spacing: 20) {
+                ForEach(imageNames, id: \.self) { imageName in
+                    Image(imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 150)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .shadow(radius: 5)
+                }
+            }
+            .padding()
+            
+            Button("1881-∞") {
                 presenter.goBack()
-                        }
+            }
+            .padding(.top, 20)
+        }
     }
-        
 }
-
-
-
-
-
