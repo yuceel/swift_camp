da393320161af99881359066a12c69bee5104e74
yuceel <muhammetyucel.diler@gmail.com>
2025-01-04 08:10:29 +0300

    add: observed object component Viper implementation

--- SwiftUI/SwiftUI-Components-App/DemoApp/Modules/Home/HomeWireframe.swift
+++ SwiftUI/SwiftUI-Components-App/DemoApp/Modules/Home/HomeWireframe.swift
@@ -386,4 +386,9 @@ extension HomeWireframe: HomeWireframeInterface {
         let popoverWireframe = PopoverWireframe()
         navigationController?.pushWireframe(popoverWireframe)
     }
+    
+    func showObservedObject() {
+        let observedObjectWireframe = ObservedObjectWireframe()
+        navigationController?.pushWireframe(observedObjectWireframe)
+    }
 }
