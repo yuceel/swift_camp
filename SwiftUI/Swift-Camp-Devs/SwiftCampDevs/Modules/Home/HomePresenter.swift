4f93350ea264eda1b21b3bbcfbf8587522dc57c1
Gürkan Fikret Günak <52853374+gurkanfikretgunak@users.noreply.github.com>
2025-01-11 10:02:15 +0300

    feat: add VIPER architecture interfaces and initializers for SwiftCampDevs

---
+++ SwiftUI/Swift-Camp-Devs/SwiftCampDevs/Modules/Home/HomePresenter.swift
@@ -0,0 +1,21 @@
+import Foundation
+import Combine
+
+final class HomePresenter: ObservableObject {
+
+    // MARK: - Private properties -
+
+    private let wireframe: HomeWireframeInterface
+
+    // MARK: - Lifecycle -
+
+    init(wireframe: HomeWireframeInterface) {
+        self.wireframe = wireframe
+
+    }
+
+    func goBack() {
+        wireframe.goBack()
+    }
+
+}
---
+++ SwiftUI/Swift-Camp-Devs/SwiftCampDevs/Modules/Home/HomeView.swift
@@ -0,0 +1,10 @@
+import Foundation
+import SwiftUI
+
+struct HomeView: View {
+
+    @ObservedObject var presenter: HomePresenter
+    var body: some View {
+      Text("Welcome to your new app.")
+    }
+}
