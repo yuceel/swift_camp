1a774491283b1c2b19ca8001f77f665099deefd9
beyzanuravsar <byznur.avsar@gmail.com>
2025-01-24 02:08:53 +0300

    update : added blue color

--- SwiftUI/Swift-Camp-Devs/SwiftCampDevs/Utils/AppColors.swift
+++ SwiftUI/Swift-Camp-Devs/SwiftCampDevs/Utils/AppColors.swift
@@ -8,7 +8,7 @@ class AppColors {
     static let darkGray = Color("DarkGray")
     static let lightGray = Color("LightGray")
     static let powderBlue = Color("PowderBlue")
+    static let blue = Color("Blue")
     static let successColor = Color("SuccessColor")
     static let warningColor = Color("WarningColor")
-    
 }
